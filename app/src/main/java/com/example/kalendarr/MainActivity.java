715d1b6027b3bb792518ce3ebce4435271804056
iburov.ca <iburov.ca@gmail.com>
2019-11-20 15:13:47 -0500

    added calendarview and onselecteddaychange

--- app/src/main/java/com/example/kalendarr/MainActivity.java
+++ app/src/main/java/com/example/kalendarr/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.kalendarr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CalendarView cv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cv = findViewById(R.id.calendarView);
+
+        cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int y, int m, int d) {
+                Toast.makeText(getApplicationContext(), "" + d + "/" + (m+1) + "/" + y, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
